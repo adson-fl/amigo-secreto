c36ffe0c9d540903557ed203febfbcf5793b1adb
Adson Leite <adsonferreira585@gmail.com>
2025-01-30 00:36:03 -0300

    criar lista

--- app.js
+++ app.js
@@ -13,12 +13,34 @@ function adicionarAmigo(){
     else{
         amigos.push(nome);
         console.log(amigos);
+
         limparCampo()
+        atualizarLista()
     }
 
 }
+function atualizarLista(){
+    // chamo a lista
+    let lista = document.getElementById('listaAmigos');
+
+    // limpar lista
+    lista.innerHTML = '';
+
+    // percorre lista 
+    for( let i = 0; i < amigos.length; i++){
+        // criando elementos na lista 
+        let amigo = document.createElement('li');
+        // cada elemento da lista recebera o nome de um amigo
+        amigo.textContent = amigos[i];
+        // inserção dos elementos na minha "ul"
+        lista.appendChild(amigo);
+    }
+    
+}
+
 // limpar campo 
 function limparCampo(){
     let nome = document.querySelector('input');
     nome.value = '';
 }
+
